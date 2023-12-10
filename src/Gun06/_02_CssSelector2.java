57852a2a6405cdff2305049f7a868d205b9660d1
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-12-11 00:20:32 +0300

    _01_CssSelector_2

--- src/Gun06/_02_CssSelector2.java
+++ src/Gun06/_02_CssSelector2.java
@@ -2,18 +2,22 @@ package Gun06;
 
 import Utlity.BaseDriver;
 import Utlity.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+public class _02_CssSelector2 extends BaseDriver {
+    @Test
+    public  void Test1() {
 
-public class _01_CssSelector extends BaseDriver {
-    public static void main(String[] args) {
 
         driver.get("https://demo.seleniumeasy.com/basic-first-form-demo.html");
         MyFunc.Bekle(2);
 
+        String mesajim = "We Are The Best";
         WebElement txtBox = driver.findElement(By.cssSelector("[id='user-message'][type='text']"));
-        txtBox.sendKeys("We Are The Best");
+        txtBox.sendKeys(mesajim);
         MyFunc.Bekle(2);
 
 
@@ -23,10 +27,12 @@ public class _01_CssSelector extends BaseDriver {
 
         WebElement msg = driver.findElement(By.cssSelector("span[id='display']"));
 
-        if (msg.getText().equals("We Are The Best"))
-            System.out.println("test passed");
-        else
-            System.out.println("test fail");
+        //   if (msg.getText().equals("We Are The Best"))
+        //       System.out.println("test passed");
+        //   else
+        //       System.out.println("test fail");
+
+        Assert.assertTrue(msg.getText().equals(mesajim));
 
 
         BekleVeKapat();
